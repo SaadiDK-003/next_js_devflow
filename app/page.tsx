793a81ffb22a5b2d0c85e954efe46afd27158e33
SaadiDK-003 <pcremote059@gmail.com>
2024-11-03 16:02:33 +0500

    little change

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-bold text-primary-500">
+    <h1 className="text-center text-3xl font-bold text-primary-500">
       Assalam-o-Alikum, Welcome to Next-Js-15 Course.
     </h1>
   );
