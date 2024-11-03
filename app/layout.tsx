7c2559a348dd80a1438f7a9bc722bf8562e34d17
SaadiDK-003 <pcremote059@gmail.com>
2024-11-03 21:23:22 +0500

    Light_Dark Theme, Shadcn & Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import React from "react";
 
+import Navbar from "@/components/navigation/navbar";
+import ThemeProvider from "@/context/Theme";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -29,11 +32,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
